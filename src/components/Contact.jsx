c5fe552cb8ff9fa940d43df33ac36272e7a5559e
Stephen <stephen.uweh@gmail.com>
2024-04-18 05:10:31 +0100

    Changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_5uk7qki', // paste your ServiceID here (you'll get one when your service is created).
+        'template_ivo07i3', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Stephen Uweh', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'stephen.uweh@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'xceXpUXSSNWJDP8VK' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./stephen-uweh.github.io/",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 })
